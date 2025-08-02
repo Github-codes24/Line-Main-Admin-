415681f4aca788bd2dd768c5f0d896d6fbb86373
Gayatri21062003 <gayatrisontakke203@gmail.com>
2025-08-02 18:18:09 +0530

    created sidebar and nav components of admin

---
+++ src/components/layout/navbar.jsx
@@ -0,0 +1,45 @@
+import {ArrowDropDown} from "@mui/icons-material";
+import {Avatar, Box, IconButton, Typography} from "@mui/material";
+import React from "react";
+
+function navbar() {
+    return (
+        <Box
+            sx={{
+                minWidth: "100vw",
+                paddingY: "8px",
+                paddingX: "16px",
+                boxSizing: "border-box",
+                display: "flex",
+                flexDirection: "row",
+                justifyContent: "space-between",
+                alignItems: "center",
+                boxShadow: "0px 2px 2px rgba(0,0,0,0.25)",
+            }}
+        >
+            {/* logo */}
+            <Typography variant="h6" color="#007E74" fontWeight={700} fontSize={"28px"}>
+                LineMan Logo
+            </Typography>
+
+            <Box
+                sx={{
+                    display: "flex",
+                    flexDirection: "row",
+                    justifyContent: "space-between",
+                    alignItems: "center",
+                    gap: "5px",
+                }}
+            >
+                {/* profile */}
+                <Avatar alt="profile">GS</Avatar>
+                {/* dropdown */}
+                <IconButton size="small" sx={{p: 0}}>
+                    <ArrowDropDown />
+                </IconButton>
+            </Box>
+        </Box>
+    );
+}
+
+export default navbar;
