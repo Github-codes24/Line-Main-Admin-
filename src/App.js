2b2811b03212ed7521c7e6efeb6235bb18c0e4b5
HarshvardhanTakale <harshvardhantakale18@gmail.com>
2025-08-21 18:49:17 +0530

    Set Limit Amount Pages Added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import React from "react";
 import { Routes, Route } from 'react-router-dom';
 import "./index.css";
-import ChargesList from "./pages/module/set-charges-of-worker/charges-list";
-import AddCommission from "./pages/module/set-charges-of-worker/add-commission";
-import ViewCommission from "./pages/module/set-charges-of-worker/view-commission";
-import EditCommission from "./pages/module/set-charges-of-worker/edit-commission";
+
+import SetLimitAmount from "./pages/module/set-limit-amount/set-limit-amount";
+import SetLimitAmount2 from "./pages/module/set-limit-amount/set-limit-amount2";
 
 
 
@@ -12,17 +11,30 @@ import EditCommission from "./pages/module/set-charges-of-worker/edit-commission
 
 function App() {
   return (
-    <Routes>
-      <Route path="" element={<ChargesList/>} />
-      <Route path="/add-commission" element={ <AddCommission/>} />
-      <Route path="/view-commission" element={<ViewCommission/>} />
-      <Route path="/edit-commission" element={<EditCommission/>} />
-
-    </Routes>
+    <>
+    <div>
+      <SetLimitAmount/>
+      <SetLimitAmount2/>
+    </div>
+    </>
   );
 }
 
 export default App;
+
+{/* <Routes>
+    //   <Route path="" element={<ChargesList/>} />
+    //   <Route path="/add-commission" element={ <AddCommission/>} />
+    //   <Route path="/view-commission" element={<ViewCommission/>} />
+    //   <Route path="/edit-commission" element={<EditCommission/>} />
+
+    // </Routes> */}
+
+// import ChargesList from "./pages/module/set-charges-of-worker/charges-list";
+// import AddCommission from "./pages/module/set-charges-of-worker/add-commission";
+// import ViewCommission from "./pages/module/set-charges-of-worker/view-commission";
+// import EditCommission from "./pages/module/set-charges-of-worker/edit-commission";
+
 
 // import CommissionList from "./pages/module/set-commission/commmission-list";
 // import AddCommission from './pages/module/set-commission/add-commission';
